3daa6b4b05550380574e1a0e65ed9844cb36ed1a
Szczepan342 <61199470+Szczepan342@users.noreply.github.com>
2020-12-06 10:48:38 +0100

    Traning exercise 2 ;

--- src/main/java/Collection/Main.java
+++ src/main/java/Collection/Main.java
@@ -2,6 +2,7 @@ package Collection;
 
 public class Main {
     public static void main(String[] args) {
-        List_traning.trainList();
+//        List_traning.trainList();
+        SetTraning.trainSet();
     }
 }
---
+++ src/main/java/Collection/SetTraning.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTraning {
+    public static void trainSet (){
+        String name1 = "Basia";
+        String name2 = "Kacper";
+        String name3 = "Janek";
+        Set<String> uniqueNames = new HashSet<>();
+        uniqueNames.add(name1);
+        uniqueNames.add(name2);
+        uniqueNames.add(name3);
+        System.out.println(uniqueNames.size());
+    }
+}
